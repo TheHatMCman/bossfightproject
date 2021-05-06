5f13be4afbbb287d5d0ba0c4ff6e798ef0a7cad4
TheHatMCman <82404513+TheHatMCman@users.noreply.github.com>
2021-05-06 13:50:51 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,37 @@
 namespace SpriteKind {
     export const bossEnemy = SpriteKind.create()
 }
-controller.combos.attachCombo("" + controller.combos.idToString(controller.combos.ID.up) + controller.combos.idToString(controller.combos.ID.A), function () {
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    if (controller.up.isPressed()) {
+        thePlayer.vy = -100
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (jumpCounter == 0) {
+        thePlayer.vy = -150
+        pause(200)
+        thePlayer.vy = -100
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
+    pause(100)
+    scene.cameraShake(4, 1000)
+    arenaOne()
+})
+controller.combos.attachCombo("" + controller.combos.idToString(controller.combos.ID.up) + controller.combos.idToString(controller.combos.ID.B), function () {
 	
 })
+function upAttack () {
+	
+}
 function stageOne () {
     tiles.setTilemap(tilemap`level3`)
 }
 function arenaOne () {
     tiles.setTilemap(tilemap`level1`)
 }
+let jumpCounter = 0
+let thePlayer: Sprite = null
 let bossOne = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -30,7 +52,7 @@ let bossOne = sprites.create(img`
     `, SpriteKind.bossEnemy)
 let playerStatusBar = statusbars.create(20, 4, StatusBarKind.Health)
 playerStatusBar.setColor(7, 2, 10)
-let thePlayer = sprites.create(img`
+thePlayer = sprites.create(img`
     . . . . . . . f f f 1 1 f . . . 
     . . . . f f f f 1 1 1 1 1 f . . 
     . . f f f 1 1 1 1 . . . 1 f . . 
@@ -49,8 +71,39 @@ let thePlayer = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 playerStatusBar.attachToSprite(thePlayer)
-let enemyStatusBar = statusbars.create(20, 4, StatusBarKind.EnemyHealth)
+let enemyStatusBar = statusbars.create(40, 4, StatusBarKind.EnemyHealth)
 enemyStatusBar.setColor(10, 2, 7)
 enemyStatusBar.attachToSprite(bossOne)
-controller.moveSprite(thePlayer)
+controller.moveSprite(thePlayer, 100, 0)
 scene.cameraFollowSprite(thePlayer)
+stageOne()
+game.onUpdate(function () {
+    if (thePlayer.isHittingTile(CollisionDirection.Bottom)) {
+        jumpCounter = 0
+    }
+})
+forever(function () {
+    thePlayer.ay = 200
+    if (thePlayer.tileKindAt(TileDirection.Left, assets.tile`myTile2`)) {
+        thePlayer.ay = 0
+        thePlayer.vy = 40
+        if (controller.A.isPressed()) {
+            thePlayer.vy = -40
+            thePlayer.vx = 100
+            if (thePlayer.isHittingTile(CollisionDirection.Bottom)) {
+                thePlayer.vx = 0
+            }
+        }
+    }
+    if (thePlayer.tileKindAt(TileDirection.Right, assets.tile`myTile3`)) {
+        thePlayer.ay = 0
+        thePlayer.vy = 40
+        if (controller.A.isPressed()) {
+            thePlayer.vy = -40
+            thePlayer.vx = -100
+        }
+        if (thePlayer.isHittingTile(CollisionDirection.Bottom)) {
+            thePlayer.vx = 0
+        }
+    }
+})
